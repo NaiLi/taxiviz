ab3c12c98360a39a53f5bdc01cf07ee64ed18a0f
Anna <anna.flisberg@gmail.com>
2015-02-17 15:38:51 +0100

    got things working on my computer, had to change the csv

--- js/map.js
+++ js/map.js
@@ -1,15 +1,26 @@
 function map() {
 
+	console.log("in function map()");
+
 	var data;
 
-	d3.csv("data/taxi_sthlm_march_2013_small.csv", function(error, data) {
+	d3.csv("data/taxi_sthlm_march_2013_small.dsv", function(error, data) {
 		self.data = data;
+
+		var test = data[1];
+		var x = data[1]["x_coord"];
+		var y = data[1]["y_coord"];
+
+		console.log(test);
+		console.log(x);
+		console.log(y);
 
 		draw(data);
 	});
 
 	function draw(data) {
-		console.log(data[0]["x_cord"]);
+		console.log("in function draw()");
+
 	}
 }
 
