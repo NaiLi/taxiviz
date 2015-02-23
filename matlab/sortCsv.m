f94bc9355a18a268980e22f884cb94d292357ae8
NaiLi <linnea.nabo@gmail.com>
2015-02-23 10:59:32 +0100

    more matlab

--- matlab/sortCsv.m
+++ matlab/sortCsv.m
@@ -1,17 +1,28 @@
 
-%%
-%String fileName = 'taxi_sthlm_march_2013_5000.csv';
-%data = csvimport(fileName);
 
-%%
+%% Read whole file
 fileName = 'taxi_sthlm_march_2013.csv';
+
+%% Create table from file
 delimiterIn = ';';
 
-formatSpec = '%u%{yyyy-MM-dd HH:mm:ss}D%f%f%c';
+%formatSpec = '%u%{yyyy-MM-dd HH:mm:ss}D%f%f%c';
+formatSpec = '%u%q%f%f%c';
 
 T = readtable(fileName, 'Delimiter', delimiterIn, 'Format', formatSpec);
 
+%% Sort table on date
 allTaxisAscendingTime = sortrows(T,2);
 
-%writetable(allTaxisAscendingTime, 'new.csv');
+
+%% Create subtables
+% Known indices:
+% First of 4 march (monday): 1128106
+% Last of 10 march (sunday): 4294860
+
+%fromFourthOfMars = allTaxisAscendingTime(1128106:13272752,:);
+weekOne = allTaxisAscendingTime(1128106:4294860,:);
+
+%% Write to new csv file
+writetable(weekOne, 'weekOne.csv');
 
