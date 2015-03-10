951b35134d223d30bc48438d3121c4d4335611b4
NaiLi <linnea.nabo@gmail.com>
2015-03-10 08:18:10 +0100

    just testing some distance calculations

--- js/main.js
+++ js/main.js
@@ -6,13 +6,14 @@ var localData = [];
 var map = new map();
 var interaction;
 var circleChart = new circleChart();
+var meanDist = new meanDist();
 
 d3.csv(file, function(data) {
 
 	//use to get entire data set:
 	//self.data = data;
 	//use if we want only one day:
-	self.data = map.getOneDay(data, new Date("2013-03-12"));
+	self.data = map.getOneDay(data, new Date("2013-03-02")); // 2 mars
 
 	for(var i=0; i<24; i++) {
 		var temp  = map.getHourOf(self.data, i);
@@ -21,5 +22,7 @@ d3.csv(file, function(data) {
 
 	interaction = new interaction();
 	map.run();
-	circleChart.run(data);
+	circleChart.run(data); //circle chart always use entire data set
+
+	meanDist.run(self.data); //whole data set
 });
---
+++ js/meanDist.js
@@ -0,0 +1,122 @@
+function meanDist() {
+	/*Calculate mean distance of a hired route
+	 *Data argument should be sorted by time first*/
+
+  this.run = function(data) {
+
+    var done = false;
+    var i;
+    var startPoint;
+    var endPoint;
+
+    //Find random car
+    var rand = Math.floor(Math.random()*data.length);
+
+    getStartAndEnd(data, rand);
+
+  }
+
+  //TODO border cases!
+  function getStartAndEnd(data, i) {
+    var car = data[i];
+    console.log(car["hired"])
+    //if we start at hired, search for false first:
+    while(car["hired"] == "t") {
+      i++;
+      if(car["id"] == data[i]["id"]) 
+        car = data[i];
+    }
+
+    //if not (or after) we want to find the next time it is hired
+    while(car["hired"] == "f") {
+      i++;
+      if(car["id"] == data[i]["id"]) 
+        car = data[i];
+    }
+
+    startPoint = car;
+
+    //Search for end of the same route
+    var temp;
+    while(car["hired"] == "t") {
+      temp = car;
+      i++;
+      if(car["id"] == data[i]["id"]) 
+        car = data[i];
+    }
+
+    endPoint = temp;
+
+    console.log("the car " + startPoint["id"] + "(" + endPoint["id"] + ") has gone from ");
+    console.log(startPoint["hired"] + " to " + endPoint["hired"] + " (not " + car["hired"] + ")")
+
+    calcDist(startPoint, endPoint);
+  }
+
+  //Calculate distance between start and end point
+  function calcDist(start, end) { //calculate euclidian distance between start and end point
+
+    var a = new google.maps.LatLng(start["y_coord"], start["x_coord"]);
+    var b = new google.maps.LatLng(end["y_coord"], end["x_coord"]);
+
+    var origin = start["y_coord"] + "," + start["x_coord"];
+    var destination = end["y_coord"] + "," + end["x_coord"];
+  
+    //fågeldistans
+    //var dist = google.maps.geometry.spherical.computeDistanceBetween (a, b);
+    //console.log(dist)
+
+    var service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix(
+    {
+      origins: [origin],
+      destinations: [destination],
+      travelMode: google.maps.TravelMode.DRIVING,/*
+      transitOptions: TransitOptions,
+      unitSystem: UnitSystem,
+      durationInTraffic: Boolean,
+      avoidHighways: Boolean,
+      avoidTolls: Boolean,*/
+    }, callback);
+
+    function callback(response, status) {
+      if (status == google.maps.DistanceMatrixStatus.OK) {
+      console.log("in callback " + response.originAddresses)
+
+      var distance = response.rows[0].elements[0].distance.text;
+      console.log("distance: " + distance);
+      // See Parsing the Results for
+      // the basics of a callback function.
+    }
+    }
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
