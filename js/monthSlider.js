47364d57feb0db7e1184074f2d79cc8823e1d811
NaiLi <linnea.nabo@gmail.com>
2015-03-11 16:59:57 +0100

    more thingies

--- js/monthSlider.js
+++ js/monthSlider.js
@@ -14,8 +14,11 @@ function monthSlider() {
 		map.run();
 	}
 
+	var x = d3.scale.linear().domain([1,31]).range([0,300]).clamp(true);
+	var xAxis = d3.svg.axis()
+                  .scale(x)
+                  .orient("top");
 
-	
 	var slide = d3.select("#monthSlider").on("input", function() {
  		 
  		var num = this.value;
@@ -24,13 +27,17 @@ function monthSlider() {
  		//console.log(new Date(d));
 
  		setLocalData(data, new Date(d)); 
-	});
+	})
+	
+	slide.append("g")
+	.call(xAxis)
+	.select(".domain");
 
 	this.setGlobalDataGlobal = function(_data, date) {
 		setLocalData(_data, date);
 	}
-
+/*
 	var slide2 = d3.select("#monthSlider2").call(d3.slider()).on("input", function() {
 		console.log("hej")
-	});
+	});*/
 }
